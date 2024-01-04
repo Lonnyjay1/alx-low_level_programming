4c80e0dfe4c79ae705c163ae1ace977d64b5d26d
Lonnyjay1 <janetakinwale95@gmail.com>
2024-01-04 02:58:49 -0800

    2-strlen.c file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _puts - prints a string to stdout
+ * _putchar - prints a string to stdout
  * @str: pointer to the string to print
  */
 void _puts(char *str)
