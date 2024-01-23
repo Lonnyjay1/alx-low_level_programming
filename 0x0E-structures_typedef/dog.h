363517b7e236a205658b63f74dc9c8bdbc1e4f56
Lonnyjay1 <janetakinwale95@gmail.com>
2024-01-22 20:59:33 -0800

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,17 @@
 #include "dog.h"
 
 /**
- * free_dog - this frees memory allocated for a struct dog
- * @d: struct dog to free
+ * init_dog - this initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
-void free_dog(dog_t *d)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
