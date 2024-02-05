5bdaa796474126e0335999d91c551966c61bdcab
Lonnyjay1 <janetakinwale95@gmail.com>
2024-02-05 15:55:16 -0800

    correction

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * listint_len - this returns number of elements in alinked list
- * @h: head pointer to traverse the list
+ * @h: head pointer to the linked list type listint_t to traverse 
  *
  * Return: the number of nodes
  */
