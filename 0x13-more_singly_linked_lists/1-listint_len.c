ad92e3b4012399d7a6b98f1d8b8f66e71574228c
Lonnyjay1 <janetakinwale95@gmail.com>
2024-02-05 15:15:06 -0800

    function that returns the number of elements in a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,8 +6,14 @@
  *
  * Return: the number of nodes
  */
+typedef struct listint_s {
+	int n;
+	struct listint_s *next;
+}listint_t;
+
 size_t listint_len(const listint_t *h);
 {
+
 	const listint_t *first = h;
 	size_t num_count = 0;
 
