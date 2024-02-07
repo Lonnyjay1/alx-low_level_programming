695f8ea63950d69cbcd292f22dd86abdbd84d104
Lonnyjay1 <janetakinwale95@gmail.com>
2024-02-06 19:19:10 -0800

    correction

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+  * reverse_listint - this reverses a list 
+  * @head: start pointer to the list
+  * Return: 0
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
